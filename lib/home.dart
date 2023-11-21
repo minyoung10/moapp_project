b30f8862cfd9a25aac97c15518757ea5940fa0b9
minyoung10 <22000374@handong.ac.kr>
2023-11-21 09:54:27 +0900

    link firebase

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
+import 'add.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,16 +12,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool isRecent = true;
-  
+
   Container transContainer() {
     return Container(
       padding: const EdgeInsets.fromLTRB(19, 12, 19, 12),
       width: 345,
       height: 90,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(9),
-        border: Border.all()
-      ),
+          borderRadius: BorderRadius.circular(9), border: Border.all()),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -28,15 +28,17 @@ class _HomePageState extends State<HomePage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text("지에스25 한동대학", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              const Text("지에스25 한동대학",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
               RichText(
                 text: const TextSpan(
-                  text: "- 5,440",
-                  style: TextStyle(fontSize: 17, color: Colors.red),
-                  children: [
-                    TextSpan(text: '원', style: TextStyle(fontSize: 14, color: Colors.black))
-                  ]
-                ),
+                    text: "- 5,440",
+                    style: TextStyle(fontSize: 17, color: Colors.red),
+                    children: [
+                      TextSpan(
+                          text: '원',
+                          style: TextStyle(fontSize: 14, color: Colors.black))
+                    ]),
               ),
             ],
           ),
@@ -46,10 +48,10 @@ class _HomePageState extends State<HomePage> {
             child: Text("잔액 44,560원", style: TextStyle(fontSize: 12)),
           )
         ],
-
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,16 +62,22 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: (){},
-            icon: const Icon(Icons.edit_rounded,
-            semanticLabel: 'edit',
-            color: Colors.black,),
+            onPressed: () {},
+            icon: const Icon(
+              Icons.edit_rounded,
+              semanticLabel: 'edit',
+              color: Colors.black,
+            ),
           ),
           IconButton(
-            onPressed: (){},
-            icon: const Icon(Icons.add_circle_outline,
-            semanticLabel: 'add',
-            color: Colors.black,),
+            onPressed: () {
+              MaterialPageRoute(builder: (context) => const AddPage());
+            },
+            icon: const Icon(
+              Icons.add_circle_outline,
+              semanticLabel: 'add',
+              color: Colors.black,
+            ),
           ),
         ],
       ),
@@ -79,14 +87,20 @@ class _HomePageState extends State<HomePage> {
           children: [
             Center(
               child: RichText(
-                text: const TextSpan(
-                  text: "500,000",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26, color: Colors.black),
-                  children: [
-                    TextSpan(text: ' 원', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14, color: Colors.black)),
-                  ]
-                )
-              ),
+                  text: const TextSpan(
+                      text: "500,000",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26,
+                          color: Colors.black),
+                      children: [
+                    TextSpan(
+                        text: ' 원',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                            color: Colors.black)),
+                  ])),
             ),
             const SizedBox(height: 27),
             const Align(
@@ -94,22 +108,22 @@ class _HomePageState extends State<HomePage> {
               child: Text("남은 잔액 | 390,000원"),
             ),
             ClipRRect(
-                borderRadius: BorderRadius.circular(9),
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: LinearPercentIndicator(
-                    padding: EdgeInsets.zero,
-                    percent: 0.37,
-                    lineHeight: 14,
-                    backgroundColor: const Color(0xFFEAE9E8),
-                    barRadius: const Radius.circular(10),
-                    progressColor: const Color(0xFFFF8787),
-                  ),
+              borderRadius: BorderRadius.circular(9),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: LinearPercentIndicator(
+                  padding: EdgeInsets.zero,
+                  percent: 0.37,
+                  lineHeight: 14,
+                  backgroundColor: const Color(0xFFEAE9E8),
+                  barRadius: const Radius.circular(10),
+                  progressColor: const Color(0xFFFF8787),
                 ),
               ),
+            ),
             const SizedBox(height: 22),
             const Align(
               alignment: Alignment.centerLeft,
@@ -131,7 +145,11 @@ class _HomePageState extends State<HomePage> {
                       isRecent = !isRecent;
                     });
                   },
-                  child: Text("최신순", style: TextStyle(fontWeight: isRecent ? FontWeight.bold : FontWeight.w400, color: Colors.black)),
+                  child: Text("최신순",
+                      style: TextStyle(
+                          fontWeight:
+                              isRecent ? FontWeight.bold : FontWeight.w400,
+                          color: Colors.black)),
                 ),
                 const Text('|'),
                 TextButton(
@@ -140,7 +158,11 @@ class _HomePageState extends State<HomePage> {
                       isRecent = !isRecent;
                     });
                   },
-                  child: Text("과거순", style: TextStyle(fontWeight: !isRecent ? FontWeight.bold : FontWeight.w400, color: Colors.black)),
+                  child: Text("과거순",
+                      style: TextStyle(
+                          fontWeight:
+                              !isRecent ? FontWeight.bold : FontWeight.w400,
+                          color: Colors.black)),
                 ),
               ],
             ),
@@ -151,10 +173,7 @@ class _HomePageState extends State<HomePage> {
                 itemCount: 5,
                 itemBuilder: (BuildContext context, int index) {
                   return Column(
-                    children: [
-                      transContainer(),
-                      const SizedBox(height: 10)
-                    ],
+                    children: [transContainer(), const SizedBox(height: 10)],
                   );
                 },
               ),
