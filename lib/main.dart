18834dc0511625785e56dc9af4c38777b9395746
minyoung10 <22000374@handong.ac.kr>
2023-11-24 10:16:34 +0900

    naver map

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await NaverMapSdk.instance.initialize(clientId: 'ya78ueg5mq');
+
   runApp(const MyApp());
 }
